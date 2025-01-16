bf1d2d354147e15f5075c36163808d7e458ffed9
vvinyl <jakubplyta01@gmail.com>
2025-01-16 22:16:39 +0100

    Content component

--- weatherapp/src/components/Content/Content.js
+++ weatherapp/src/components/Content/Content.js
@@ -1,58 +1,128 @@
 // import { Container, Row, Col, InputGroup, Form, Button } from "react-bootstrap";
-import './Content.Module.css';
-import {Card, Button, InputGroup, Form} from 'react-bootstrap'
-import {useState} from 'react';
+import "./Content.Module.css";
+import { Card, Button, Form, Row, Col, Container } from "react-bootstrap";
+import { useState } from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
+// gify
+import Rain from "../../assets/gifs/drizzle.gif";
+import Clear from "../../assets/gifs/clear.gif";
+import Clouds from "../../assets/gifs/clouds.gif";
+import Foggy from "../../assets/gifs/fog.gif";
+import Snow from "../../assets/gifs/snow.gif";
 
 export const Content = () => {
-    const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
 
-    const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
 
-    const handleSearch = async () => {
-        if(!city) {
-            return;
-        }
-
-        try {
-            const apiKey = 'f6f720a0ef65ae2ccc4a2dbecd1a0ef3';
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
-
-            if(!response.ok){
-                throw new Error("Nie udało się pobrac danych pogodowych.")
-            }
-
-            const data = await response.json();
-            setWeatherData(data);
-        }catch(err){
-            console.error("Błąd pobierania danych pogodowych: ", err);
-            setWeatherData(null)
-        }
+  // funkcja obsługująca szukajkę wysyłająca żądanie do API
+  const handleSearch = async () => {
+    if (!city) {
+      return;
     }
 
+    try {
+      const apiKey = "f6f720a0ef65ae2ccc4a2dbecd1a0ef3";
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      );
 
+      if (!response.ok) {
+        throw new Error("Nie udało się pobrac danych pogodowych.");
+      }
+
+      const data = await response.json();
+      setWeatherData(data);
+      console.log(data);
+    } catch (err) {
+      console.error("Błąd pobierania danych pogodowych: ", err);
+      setWeatherData(null);
+    }
+  };
+
+  // funkcja zarządająca gifami
+  const getGif = () => {
+    if (!weatherData || !weatherData.weather || !weatherData.weather[0]) {
+      return null;
+    }
+
+    const main = weatherData.weather[0].main;
+
+    switch (main) {
+      case "Rain":
+        return Rain;
+      case "Snow":
+        return Snow;
+      case "Drizzle":
+        return Rain;
+      case "Clear":
+        return Clear;
+      case "Clouds":
+        return Clouds;
+      case "Mist":
+        return Foggy;
+      default:
+        return null;
+    }
+  };
+
+  const weatherGif = getGif();
 
   return (
-    <Card className="mainCard">
-        <Card.Header>
-        <InputGroup className="mb-3">
-               <Form.Control
-                 placeholder="Wpisz miasto"
-                 value={city}
-                 onChange={(e) => setCity(e.target.value)}
-               />
-               <Button onClick={handleSearch}>
-                 Szukaj
-               </Button>
-             </InputGroup>
-        </Card.Header>
+    <Container className="card">
+      <Card className="mainCard">
         <Card.Body>
-        {weatherData && (
-            <>
-                <p>Temp: {weatherData.main.temp}</p>
-                <p>Wiatr: {weatherData.wind.speed}</p>
-            </>
-        )}
+          <Row className="mb-4">
+            <Col xs={12} md={6}>
+              <Form.Control
+                placeholder="City"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                className="input"
+              />
+            </Col>
+            <Col xs={12} md={6} className="d-flex align-items-center">
+              <Button
+                onClick={handleSearch}
+                className="button"
+              >
+                Search
+                <FaMagnifyingGlass className="icon"/>
+              </Button>
+            </Col>
+          </Row>
+
+          {weatherData && (
+            <Row>
+              <Col xs={12} md={6} className="details">
+                <h3 style={{ fontWeight: "bold" }}>
+                  {weatherData.name.toUpperCase()}
+                </h3>
+                <p>Temperature: {weatherData.main.temp} °C</p>
+                <p>Wind: {weatherData.wind.speed} m/s</p>
+                <p>Pressure: {weatherData.main.pressure} hPa</p>
+              </Col>
+              <Col
+                xs={12}
+                md={6}
+                className="d-flex justify-content-center align-items-center gifColumn"
+              >
+                {weatherGif && (
+                  <img
+                    src={weatherGif}
+                    alt={weatherData.weather[0].main}
+                    style={{
+                      borderRadius: "50%",
+                      width: 300,
+                      height: 300}}
+                  />
+                )}
+              </Col>
+            </Row>
+          )}
         </Card.Body>
-    </Card>
+      </Card>
+    </Container>
   );
 };
--- weatherapp/src/components/Header/Header.js
+++ weatherapp/src/components/Header/Header.js
@@ -9,11 +9,11 @@ export const Header = () => {
   return (
     <Container fluid className={styles.containerTop}>
         <Row className="justify-content-center">
-        <Col xs={12}>
+            <Col>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="3rem"
-                height="3rem"
+                width="6rem"
+                height="6rem"
                 fill="white"
                 className="bi bi-clouds"
                 viewBox="0 0 16 16"
@@ -21,14 +21,10 @@ export const Header = () => {
                 <path d="M16 7.5a2.5 2.5 0 0 1-1.456 2.272 3.5 3.5 0 0 0-.65-.824 1.5 1.5 0 0 0-.789-2.896.5.5 0 0 1-.627-.421 3 3 0 0 0-5.22-1.625 5.6 5.6 0 0 0-1.276.088 4.002 4.002 0 0 1 7.392.91A2.5 2.5 0 0 1 16 7.5" />
                 <path d="M7 5a4.5 4.5 0 0 1 4.473 4h.027a2.5 2.5 0 0 1 0 5H3a3 3 0 0 1-.247-5.99A4.5 4.5 0 0 1 7 5m3.5 4.5a3.5 3.5 0 0 0-6.89-.873.5.5 0 0 1-.51.375A2 2 0 1 0 3 13h8.5a1.5 1.5 0 1 0-.376-2.953.5.5 0 0 1-.624-.492z" />
             </svg>
-        </Col>
-        </Row>
-        <Row className="justify-content-center">
-        <Col xs={12}>
             <p className={`${styles.header}`}>
                 {fullText}
             </p>
-        </Col>
+            </Col>
         </Row>
     </Container>
   );
